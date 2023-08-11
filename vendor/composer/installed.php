6f496226fae1c01baa1f652dffccc6c9f79ec70d
djamelkadi <djamel.k@gmail.com>
2023-08-11 11:08:48 -0500

    Analyze code with chat gpt
    Analyze code with chat gpt
    
    Co-Authored-By: djamelkad <142038173+djamelkad@users.noreply.github.com>

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '10152b4610b0412132e364490a5af5a2244852cb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '10152b4610b0412132e364490a5af5a2244852cb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v10.3.0',
+            'version' => '10.3.0.0',
+            'reference' => '417d66a5b27a5ec53a706a346b05bfe170043d52',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
